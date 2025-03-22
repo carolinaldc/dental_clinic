1c7cb64e51a77d6ee2e6e2311c5e3ba011f95255
maria20040717 <maria.martinezvillarrasa@usp.ceu.es>
2025-03-22 21:04:19 +0100

    JDBCManager class created, lib folder added, packaged DentalClinicJDBC

---
+++ src/DentalClinicJDBC/JDBCManager.java
@@ -0,0 +1,13 @@
+package DentalClinicJDBC;
+
+import java.sql.Connection;
+
+public class JDBCManager {
+	
+	private Connection c = null;
+	
+	public JDBCManager() {
+		
+	}
+
+}
--- src/dental_clinic/menu.java
+++ src/dental_clinic/menu.java
@@ -1,11 +1,18 @@
 package dental_clinic;
 
+import DentalClinicJDBC.JDBCManager;
+
 public class menu {
 
+	private static JDBCManager jdbcManager;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		
+		jdbcManager = new JDBCManager();
+		
 	}
+	
 
 }
