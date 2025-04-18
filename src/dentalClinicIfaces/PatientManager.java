ca2d4c18861ba20d8d2ab0990794f7e5f0af37bb
ClaudiaBartolome <claudiamaria.bartolomelopez@usp.ceu.es>
2025-04-19 00:49:13 +0200

    Creation of Patient and Clinician Ifaces. JDBCPatientManager

---
+++ src/dentalClinicIfaces/PatientManager.java
@@ -0,0 +1,5 @@
+package dentalClinicIfaces;
+
+public interface PatientManager {
+
+}
--- src/dentalClinicJDBC/JDBCPatientManager.java
+++ src/dentalClinicJDBC/JDBCPatientManager.java
@@ -1,5 +1,37 @@
 package dentalClinicJDBC;
 
-public class JDBCPatientManager {
+import dentalClinicPOJOS.Patient;
+import java.sql.PreparedStatement;
+
+import dentalClinicIfaces.PatientManager;
+
+public class JDBCPatientManager implements PatientManager{
+	private JDBCPatientManager manager;
+	
+	private JDBCPatientManager(JDBCManager m) {
+		this.manager = m;
+	}
+	
+	public void addPatient(Patient p) {
+		try {
+			String sql = "INSERT INTO Patients (name, surname, dob, creditcard, email, telephone, urgency, id_clinician) VALUES(?,?,?,?,?,?,?,?)";
+			
+			PreparedStatement prep = manager.getConnection().prepareStatement(sql);
+			
+			prep.setString(1, p.getName());
+			prep.setString(2, p.getSurname());
+			prep.setDate(3, p.getDob());
+			prep.setInt(4, p.getCredit_card());
+			prep.setString(5, p.getEmail());
+			prep.setInt(6, p.getPhone());
+			prep.setEmergency(7, p.getUrgency());
+			prep.setInt(8, p.getClinician_id);
+			
+			prep.executeUpdate();
+
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
