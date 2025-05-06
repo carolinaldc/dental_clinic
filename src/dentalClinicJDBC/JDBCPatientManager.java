e2c6b7d2035f70449306ec7d333eb4a4067f4817
Claudia Blanco Alvarez <159258364+Claaublanco4@users.noreply.github.com>
2025-05-06 15:06:32 +0200

    Update JDBCPatientManager.java
    Ya he conseguido añadir los metodos !!!!

--- src/dentalClinicJDBC/JDBCPatientManager.java
+++ src/dentalClinicJDBC/JDBCPatientManager.java
@@ -34,7 +34,26 @@ public class JDBCPatientManager implements PatientManager{
 			e.printStackTrace();
 		}
 		
-		
+		 @Override
+    public Patient getPatientById(int id) {
+        try (PreparedStatement ps = connection.prepareStatement(
+                "SELECT * FROM patients WHERE id = ?")) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new Patient(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("dni"),
+                        rs.getString("phone"),
+                        rs.getDate("birth_date")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 	}
 	
 	@Override
@@ -42,5 +61,51 @@ public class JDBCPatientManager implements PatientManager{
 		//TODO method
 		
 	}
+	@Override
+    public List<Patient> listPatients() {
+        List<Patient> list = new ArrayList<>();
+        try (Statement st = connection.createStatement()) {
+            ResultSet rs = st.executeQuery("SELECT * FROM patients");
+            while (rs.next()) {
+                Patient p = new Patient(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("dni"),
+                        rs.getString("phone"),
+                        rs.getDate("birth_date")
+                );
+                list.add(p);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 
+ @Override
+    public void deletePatient(int id) {
+        try (PreparedStatement ps = connection.prepareStatement("DELETE FROM patients WHERE id = ?")) {
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updatePatient(Patient p) {
+        try (PreparedStatement ps = connection.prepareStatement(
+                "UPDATE patients SET name=?, dni=?, phone=?, birth_date=? WHERE id=?")) {
+            ps.setString(1, p.getName());
+            ps.setString(2, p.getDni());
+            ps.setString(3, p.getPhone());
+            ps.setDate(4, new java.sql.Date(p.getBirthDate().getTime()));
+            ps.setInt(5, p.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+}
 }
