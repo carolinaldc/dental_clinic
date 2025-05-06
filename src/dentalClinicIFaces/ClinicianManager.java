fff9eff553285f411011e8983ef83dab6ecd31ab
Claudia Blanco Alvarez <159258364+Claaublanco4@users.noreply.github.com>
2025-05-06 15:09:11 +0200

    Update ClinicianManager.java
    He completado los metodos de la interfaz

--- src/dentalClinicIFaces/ClinicianManager.java
+++ src/dentalClinicIFaces/ClinicianManager.java
@@ -4,5 +4,10 @@ import dentalClinicPOJOS.Clinician;
 
 public interface ClinicianManager {
 	void getClinician(String email);
-	void addClinician(Clinician clinician);
+	 void addClinician(Clinician clinician);
+   	 Clinician getClinicianById(int id);
+   	 List<Clinician> getAllClinicians();
+    	void deleteClinician(int id);
+    	void updateClinician(Clinician cl);
 }
+
