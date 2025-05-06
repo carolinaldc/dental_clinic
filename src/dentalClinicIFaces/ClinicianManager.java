a4a1590da2d39b21b6cc772edc6cce9a82318a09
claaublanco4 <claudia.blancoalvarez@usp.ceu.es>
2025-05-06 14:59:09 +0200

    He añadido los metodos a clinician manager y patient manager

--- src/dentalClinicIFaces/ClinicianManager.java
+++ src/dentalClinicIFaces/ClinicianManager.java
@@ -1,5 +1,13 @@
 package dentalClinicIFaces;
+import java.util.List;
+import dentalClinicPOJOs.Clinician;
 
 public interface ClinicianManager {
+	
+	    void addClinician(Clinician c);
+	    List<Clinician> listClinicians();
+	    Clinician getClinicianById(int id);
+	    void deleteClinician(int id);
+	    void updateClinician(Clinician c);
+	}
 
-}
--- src/dentalClinicIFaces/PatientManager.java
+++ src/dentalClinicIFaces/PatientManager.java
@@ -1,5 +1,12 @@
 package dentalClinicIFaces;
 
-public interface PatientManager {
+import java.util.List;
+import dentalClinicPOJOS.Patient;
 
+public interface PatientManager {
+    void addPatient(Patient p);
+    List<Patient> listPatients();
+    Patient getPatientById(int id);
+    void deletePatient(int id);
+    void updatePatient(Patient p);
 }
