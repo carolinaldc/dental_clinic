b58be87649fb00aca4464a91cbf00bc63191baa1
claaublanco4 <claudia.blancoalvarez@usp.ceu.es>
2025-05-11 16:14:31 +0200

    Creada interfaz MaterialManager

---
+++ src/dentalClinicIFaces/MaterialManager.java
@@ -0,0 +1,19 @@
+package dentalClinicIFaces;
+
+import java.util.List;
+
+import dentalClinicPOJOS.Material;
+
+public interface MaterialManager {
+
+    void addMaterial(Material material);
+
+    List<Material> getAllMaterials();
+
+    void updateMaterial(Material material);
+
+    void deleteMaterial(int materialId);
+
+    void linkMaterialToTreatment(int materialId, int treatmentId);
+
+}
