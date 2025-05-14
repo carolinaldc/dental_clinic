1525e9cd8a0ed475a20713bafaeafcde27c44168
claaublanco4 <claudia.blancoalvarez@usp.ceu.es>
2025-05-14 09:35:07 +0200

    Creada interfaz TreatmentMaterialManager.java

--- src/dentalClinicIFaces/TreatmentMaterialManager.java
+++ src/dentalClinicIFaces/TreatmentMaterialManager.java
@@ -1,5 +1,21 @@
 package dentalClinicIFaces;
 
+
+import java.util.List;
+
+import dentalClinicPOJOS.TreatmentMaterial;
+import dentalClinicPOJOS.Treatment;
+import dentalClinicPOJOS.Material;
+
+
 public interface TreatmentMaterialManager {
 
-}
+
+	   void addMaterialToTreatment(int treatmentId, int materialId);
+	   List<Material> getMaterialsForTreatment(int treatmentId);
+	   boolean isMaterialAssignedToTreatment(int treatmentId, int materialId);
+	   void updateMaterialQuantityInTreatment(int treatmentId, int materialId, int quantity);
+	   List<Treatment> getTreatmentsForMaterial(int materialId);
+	}
+
+
