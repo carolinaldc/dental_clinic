ea7907cc8d724b2292ee80841409b994efde0427
claaublanco4 <claudia.blancoalvarez@usp.ceu.es>
2025-05-14 09:35:46 +0200

    Creada la interfaz patientsClinicianManager

--- src/dentalClinicIFaces/PatientsClinicianManager.java
+++ src/dentalClinicIFaces/PatientsClinicianManager.java
@@ -1,5 +1,23 @@
 package dentalClinicIFaces;
 
+import java.util.List;
+import dentalClinicPOJOS.Patients_Clinician;
+
 public interface PatientsClinicianManager {
 
+	
+    void addPatientClinician(Patients_Clinician pc);
+
+    List<Patients_Clinician> getAllPatientsClinicians();
+
+    
+    Patients_Clinician getPatientClinicianById(int id);
+
+    
+    void updatePatientClinician(Patients_Clinician pc);
+
+   
+    void deletePatientClinician(int id);
+	
+	
 }
