e4f191902abc829207b394860f2de3d9748ceaa9
maria20040717 <maria.martinezvillarrasa@usp.ceu.es>
2025-05-22 10:02:07 +0200

    changes done to connect new db

--- src/dentalClinicJDBC/JDBCManager.java
+++ src/dentalClinicJDBC/JDBCManager.java
@@ -16,7 +16,7 @@ public class JDBCManager {
 		try {
 			
 			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:./db/Dental_Clinic.db");
+			c = DriverManager.getConnection("jdbc:sqlite:./db/DentalClinic.db");
 			c.createStatement().execute("PRAGMA foreign_keys=ON");
 			
 		
@@ -37,6 +37,7 @@ public class JDBCManager {
 		try {
 			
 			Statement stmt = c.createStatement();
+
 			
 			String sql = "CREATE TABLE Clinicians ("
 					+ "clinician_id INTEGER PRIMARY KEY AUTOINCREMENT,"
@@ -47,6 +48,8 @@ public class JDBCManager {
 					+ "email TEXT)";
 			stmt.executeUpdate(sql);
 			
+
+			
 			sql = "CREATE TABLE Patients ("
 					+ "patient_id INTEGER PRIMARY KEY AUTOINCREMENT,"
 					+ "name TEXT,"
@@ -54,120 +57,79 @@ public class JDBCManager {
 					+ "dob TEXT,"
 					+ "phone INTEGER,"
 					+ "email TEXT,"
-					+ "emergency TEXT,"
 					+ "credit_card INTEGER)";
 			stmt.executeUpdate(sql);
+
 			
 			sql = "CREATE TABLE Materials ("
 					+ "materials_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-					+ "name TEXT,"
-					+ "stock INTEGER)";
-			stmt.executeUpdate(sql);
-			
-			sql = "CREATE TABLE Rooms ("
-					+ "room_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-					+ "status TEXT)";
+					+ "supplier_id INTEGER REFERENCES Suppliers(supplier_id) NOT NULL,"
+					+ "name TEXT)";
 			stmt.executeUpdate(sql);
 
+			
 			sql = "CREATE TABLE Treatments ("
 					+ "treatment_id INTEGER PRIMARY KEY AUTOINCREMENT,"
 					+ "name TEXT,"
 					+ "description TEXT,"
-					+ "price NUMERIC,"
-					+ "room_id INTEGER REFERENCES Rooms(room_id) NOT NULL)";
+					+ "price NUMERIC)";
 			stmt.executeUpdate(sql);
 
-			sql = "CREATE TABLE Clinicians_treatments ("
-					+ "clinician_appointment_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-					+ "treatment_id INTEGER REFERENCES Treatments(treatment_id) NOT NULL,"
-					+ "clinician_id INTEGER REFERENCES Clinicians(clinician_id) NOT NULL)";
-			stmt.executeUpdate(sql);
-
-			sql = "CREATE TABLE Patient_treatments ("
-					+ "patient_appointment_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+			
+			sql = "CREATE TABLE Appointments ("
+					+ "appointment_id INTEGER PRIMARY KEY AUTOINCREMENT,"
 					+ "patient_id INTEGER REFERENCES Patients(patient_id) NOT NULL,"
 					+ "treatment_id INTEGER REFERENCES Treatments(treatment_id) NOT NULL,"
-					+ "date DATE,"
+					+ "clinician_id INTEGER REFERENCES Clinicians(clinician_id) NOT NULL,"
+					+ "date Date,"
 					+ "comments TEXT)";
 			stmt.executeUpdate(sql);
-			
-			sql = "CREATE TABLE Patients_clinicians ("
-					+ "patient_id INTEGER REFERENCES Patients(patient_id) NOT NULL,"
-					+ "clinician_id INTEGER REFERENCES Clinicians(clinician_id) NOT NULL,"
-					+ "date TEXT,"
-					+ "visit_info TEXT,"
-					+ "PRIMARY KEY(patient_id, clinician_id))";
-			stmt.executeUpdate(sql);
-			
-			sql = "CREATE TABLE Payments ("
-					+ "payment_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-					+ "amount NUMERIC,"
-					+ "payment_date DATE,"
-					+ "payment_method TEXT,"
-					+ "status TEXT)";
-			stmt.executeUpdate(sql);
+
 			
 			sql = "CREATE TABLE Treatment_materials ("
-					+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
 					+ "treatment_id INTEGER REFERENCES Treatments(treatment_id) NOT NULL,"
 					+ "materials_id INTEGER REFERENCES Materials(materials_id) NOT NULL,"
-					+ "description TEXT,"
-					+ "tools TEXT)";
+					+ "PRIMARY KEY (treatment_id, materials_id))";
 			stmt.executeUpdate(sql);	
+
 			
 			sql = "CREATE TABLE Suppliers (" 
 					+ "supplier_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-					+ "name TEXT,"
-					+ "surname TEXT,"
+					+ "supplierName TEXT,"
 					+ "phone INTEGER,"
-					+ "email TEXT,"
-					+ "materials_id INTEGER REFERENCES Materials(materials_id) NOT NULL)";
+					+ "email TEXT)";
 			stmt.executeUpdate(sql);
 
 		
-			sql = "INSERT INTO Patients (patient_id, name, surname, dob, phone, email, emergency, credit_card) "
-					+ "VALUES (2341, 'Jorge', 'Gonzalez', '1992-06-23', '+34684027863', 'jorgegonzalez@gmail.com', 'MEDIUM', '5223935698626682')";
+			sql = "INSERT INTO Patients (patient_id, name, surname, dob, phone, email, credit_card) "
+					+ "VALUES (2341, 'Jorge', 'Gonzalez', '1992-06-23', '34684027863', 'jorgegonzalez@gmail.com', '5223935698626682')";
 			stmt.executeUpdate(sql);
 
 			
 			sql = "INSERT INTO Clinicians (clinician_id, name, surname, specialty, phone, email) "
-					+ "VALUES (7823, 'Julia', 'Velazquez', 'Surgery', '+34692438522', 'juliavelazquez@gmail.com')";
+					+ "VALUES (7823, 'Julia', 'Velazquez', 'Surgery', '34692438522', 'juliavelazquez@gmail.com')";
 			stmt.executeUpdate(sql);
 
-			sql = "INSERT INTO Rooms (room_id, status) "
-					+ "VALUES (2823, 'AVAILABLE')";
-			stmt.executeUpdate(sql);
 
 			sql = "INSERT INTO Treatments (treatment_id, name, description, price, room_id) "
 					+ "VALUES (2003, 'Fenestration', 'Palate surgery', 200.00, 2823)";
 			stmt.executeUpdate(sql);
 
-			sql = "INSERT INTO Treatment_materials (treatment_id, materials_id, description, tools) "
-					+ "VALUES (2003, 5053, 'Used to reach certain spots in the mouth', 'Dental mirror')";
+			sql = "INSERT INTO Appointments (appointment_id, patient_id, treatment_id, clinician_id, date, comments) "
+					+ "VALUES (2003, 5053, 1234, 2345, 2025-12-01, 'tooth extraction')";
 			stmt.executeUpdate(sql);
 
-			sql = "INSERT INTO Suppliers (supplier_id, name, surname, phone, email, materials_id) "
-					+ "VALUES (8963, 'Roberto', 'Hernandez', '+34623985233', 'robertohernandez@gmail.com', 5053)";
+			sql = "INSERT INTO Suppliers (supplier_id, supplierName, phone, email) "
+					+ "VALUES (8963, 'CorteIngles', '34623985233', 'robertohernandez@gmail.com')";
 			stmt.executeUpdate(sql);
 
-			sql = "INSERT INTO Payments (payment_id, amount, payment_date, payment_method, status) "
-					+ "VALUES (9823, 200.00, '2025-02-05', 'CARD', 'PENDING')";
+
+			sql = "INSERT INTO Materials (materials_id, supplier_id ,name) "
+					+ "VALUES (4421, 1234 ,'Dental mirror')";
 			stmt.executeUpdate(sql);
 
-			sql = "INSERT INTO Patients_clinicians (patient_id, clinician_id, date, visit_info) "
-					+ "VALUES (2341, 7823, '2025-02-05', 'Fenestration')";
-			stmt.executeUpdate(sql);
-
-			sql = "INSERT INTO Clinicians_treatments (clinician_appointment_id, treatment_id, clinician_id) "
-					+ "VALUES (8923, 2003, 7823)";
-			stmt.executeUpdate(sql);
-
-			sql = "INSERT INTO Materials (materials_id, name, stock) "
-					+ "VALUES (4421, 'Dental mirror', 160)";
-			stmt.executeUpdate(sql);
-
-			sql = "INSERT INTO Patient_treatments (Patient_appointment_id, patient_id, treatment_id, date, comments) "
-					+ "VALUES (9873, 2341, 2003, '2025-02-05', 'Palate surgery')";
+			sql = "INSERT INTO Treatment_materials (treatment_id, materials_id) "
+					+ "VALUES (9873, 2341)";
 			stmt.executeUpdate(sql);
 
 			System.out.println("Tables created and default values inserted");
